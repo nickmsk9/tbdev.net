71c481ee03918d8d3e19572fb6ba1d514ef19ace
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2025-12-10 16:18:20 +0300

    Добавлена функция safe_number_format() - обрабатывает NULL значения перед форматированием
    Использование COALESCE() в SQL-запросах - преобразует NULL в 0 на уровне базы данных:
    
    COALESCE(SUM(condition), 0) вместо SUM(condition)
    
    COALESCE(COUNT(*), 0) вместо COUNT(*)
    
    Исправлены имена переменных:
    
    $seeders_formatted и $leechers_formatted для отформатированных значений
    
    $seeders и $leechers остались целыми числами для расчета соотношения
    
    Обработка NULL в запросе внешних сидеров - добавлен COALESCE() в SQL запрос

--- blocks/block-stats.php
+++ blocks/block-stats.php
@@ -1,40 +1,77 @@
 <?php
 
-/* ToDo:
- * Make cache in this block
-*/
-
 if (!defined('BLOCK_FILE')) {
- Header("Location: ../index.php");
- exit;
+    Header("Location: ../index.php");
+    exit;
 }
 
 global $tracker_lang, $ss_uri, $maxusers, $pic_base_url;
 
-$registered = number_format(get_row_count("users"));
-$unverified = number_format(get_row_count("users", "WHERE status='pending'"));
-$male = number_format(get_row_count("users", "WHERE gender='1'"));
-$female = number_format(get_row_count("users", "WHERE gender='2'"));
-$torrents = number_format(get_row_count("torrents"));
-$dead = number_format(get_row_count("torrents", "WHERE visible='no'"));
-$seeders = get_row_count("peers", "WHERE seeder='yes'");
-$leechers = get_row_count("peers", "WHERE seeder='no'");
-$row = mysqli_fetch_row(sql_query('SELECT SUM(seeders), SUM(leechers) FROM torrents_scrape'));
+// Вспомогательная функция для безопасного форматирования
+function safe_number_format($value) {
+    return number_format($value ?? 0);
+}
+
+// Получаем все данные пользователей одним запросом
+$users_query = sql_query("
+    SELECT 
+        COUNT(*) as total,
+        COALESCE(SUM(status='pending'), 0) as unverified,
+        COALESCE(SUM(gender='1'), 0) as male,
+        COALESCE(SUM(gender='2'), 0) as female,
+        COALESCE(SUM(warned='yes'), 0) as warned_users,
+        COALESCE(SUM(enabled='no'), 0) as disabled,
+        COALESCE(SUM(class=" . UC_UPLOADER . "), 0) as uploaders,
+        COALESCE(SUM(class=" . UC_VIP . "), 0) as vip
+    FROM users
+");
+$users_stats = mysqli_fetch_assoc($users_query);
+
+// Получаем все данные торрентов одним запросом
+$torrents_query = sql_query("
+    SELECT 
+        COALESCE(COUNT(*), 0) as total_torrents,
+        COALESCE(SUM(visible='no'), 0) as dead
+    FROM torrents
+");
+$torrents_stats = mysqli_fetch_assoc($torrents_query);
+
+// Получаем все данные пиров одним запросом
+$peers_query = sql_query("
+    SELECT 
+        COALESCE(SUM(seeder='yes'), 0) as seeders,
+        COALESCE(SUM(seeder='no'), 0) as leechers
+    FROM peers
+");
+$peers_stats = mysqli_fetch_assoc($peers_query);
+
+// Внешние сидеры/личеры
+$row = mysqli_fetch_row(sql_query('SELECT COALESCE(SUM(seeders), 0), COALESCE(SUM(leechers), 0) FROM torrents_scrape'));
 list($external_seeders, $external_leechers) = array_map(function($value) {
     return number_format($value ?? 0);
 }, $row);
-$warned_users = number_format(get_row_count("users", "WHERE warned = 'yes'"));
-$disabled = number_format(get_row_count("users", "WHERE enabled = 'no'"));
-$uploaders = number_format(get_row_count("users", "WHERE class = ".UC_UPLOADER));
-$vip = number_format(get_row_count("users", "WHERE class = ".UC_VIP));
-if ($leechers == 0)
-  $ratio = 0;
-else
-  $ratio = round($seeders / $leechers * 100);
-$peers = number_format($seeders + $leechers);
-$seeders = number_format($seeders);
-$leechers = number_format($leechers);
 
+// Форматируем данные
+$registered = safe_number_format($users_stats['total']);
+$unverified = safe_number_format($users_stats['unverified']);
+$male = safe_number_format($users_stats['male']);
+$female = safe_number_format($users_stats['female']);
+$torrents = safe_number_format($torrents_stats['total_torrents']);
+$dead = safe_number_format($torrents_stats['dead']);
+$seeders = (int)$peers_stats['seeders'];
+$leechers = (int)$peers_stats['leechers'];
+$warned_users = safe_number_format($users_stats['warned_users']);
+$disabled = safe_number_format($users_stats['disabled']);
+$uploaders = safe_number_format($users_stats['uploaders']);
+$vip = safe_number_format($users_stats['vip']);
+
+// Рассчитываем соотношение
+$ratio = $leechers == 0 ? 0 : round($seeders / $leechers * 100);
+$peers = safe_number_format($seeders + $leechers);
+$seeders_formatted = safe_number_format($seeders);
+$leechers_formatted = safe_number_format($leechers);
+
+// Генерация HTML
 $content .= "<table width=\"100%\" class=\"main\" border=\"0\" cellspacing=\"0\" cellpadding=\"2\"><td align=\"center\">
 <table class=\"main\" border=\"1\" cellspacing=\"0\" cellpadding=\"5\">
 
@@ -55,8 +92,8 @@ $content .= "<table width=\"100%\" class=\"main\" border=\"0\" cellspacing=\"0\"
 <tr><td class=\"rowhead\">{$tracker_lang['tracker_dead_torrents']}</td><td align=\"right\">{$dead}</td></tr>
 <tr><td class=\"rowhead\">{$tracker_lang['tracker_peers']}</td><td align=\"right\">{$peers}</td></tr>";
 if (isset($peers)) {
-$content .= "<tr><td class=\"rowhead\">{$tracker_lang['tracker_seeders']}&nbsp;&nbsp;<img src=\"./themes/$ss_uri/images/arrowup.gif\" border=\"0\" align=\"absbottom\"></td><td align=\"right\">{$seeders}</td></tr>
-<tr><td class=\"rowhead\">{$tracker_lang['tracker_leechers']}&nbsp;&nbsp;<img src=\"./themes/$ss_uri/images/arrowdown.gif\" border=\"0\" align=\"absbottom\"></td><td align=\"right\">{$leechers}</td></tr>
+$content .= "<tr><td class=\"rowhead\">{$tracker_lang['tracker_seeders']}&nbsp;&nbsp;<img src=\"./themes/$ss_uri/images/arrowup.gif\" border=\"0\" align=\"absbottom\"></td><td align=\"right\">{$seeders_formatted}</td></tr>
+<tr><td class=\"rowhead\">{$tracker_lang['tracker_leechers']}&nbsp;&nbsp;<img src=\"./themes/$ss_uri/images/arrowdown.gif\" border=\"0\" align=\"absbottom\"></td><td align=\"right\">{$leechers_formatted}</td></tr>
 <tr><td class=\"rowhead\">{$tracker_lang['tracker_seed_peer']}</td><td align=\"right\">{$ratio}</td></tr>";
 }
 
