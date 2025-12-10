c513d1f55dfcf10932432c0315045d5f04b327c6
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2025-12-10 12:41:56 +0300

    корректировка отображения блоков

--- include/blocks.php
+++ include/blocks.php
@@ -6,19 +6,29 @@ if(!defined('IN_TRACKER'))
 
 function render_blocks($blockfile, $blocktitle, $content, $bid, $bposition, $allow_hide) {
     global $showbanners, $allow_block_hide;
+    global $foot;
     
     if ($blockfile != "") {
-        if (file_exists("blocks/" . $blockfile)) {
+        if (file_exists("blocks/".$blockfile)) {
             if (!defined('BLOCK_FILE'))
                 define('BLOCK_FILE', 1);
-            require("blocks/" . $blockfile);
+            
+            // Используем буферизацию вывода для захвата содержимого блока
+            ob_start();
+            include("blocks/".$blockfile);
+            $block_content = ob_get_clean();
+            
+            // Если блок вернул содержимое, используем его
+            if (!empty($block_content)) {
+                $content = $block_content;
+            }
         } else {
-            $content = "<center>Файл блока не найден!</center>";
+            $content = "<center>Существует проблема с этим блоком!</center>";
         }
     }
 
-    if (!((isset($content) AND !empty($content)))) {
-        $content = "<center>Содержимое блока пустое!</center>";
+    if (empty($content)) {
+        $content = "<center>Существует проблема с этим блоком!</center>";
     }
 
     if ($allow_block_hide && ($allow_hide || get_user_class() >= UC_ADMINISTRATOR)) {
@@ -40,32 +50,39 @@ function render_blocks($blockfile, $blocktitle, $content, $bid, $bposition, $all
 }
 
 function themesidebox($title, $content, $pos) {
-    global $blockfile, $b_id, $ss_uri, $tracker_lang;
+    global $blockfile, $b_id, $ss_uri;
     static $bl_mass = [];
+    
+    $func = 'echo';
+    $func2 = '';
     
     // Определяем имя блока
     if ($pos == "s" || $pos == "o") {
-        $bl_name = empty($blockfile) ? "fly-block-" . $b_id : "fly-" . str_replace(".php", "", $blockfile);
+        $bl_name = empty($blockfile) ? "fly-block-".$b_id : "fly-".str_replace(".php", "", $blockfile);
     } else {
-        $bl_name = empty($blockfile) ? "block-" . $b_id : str_replace(".php", "", $blockfile);
+        $bl_name = empty($blockfile) ? "block-".$b_id : str_replace(".php", "", $blockfile);
     }
     
     // Проверяем существование кастомного файла блока
-    if (!isset($bl_mass[$bl_name]['m'])) {
-        $bl_mass[$bl_name]['m'] = file_exists("themes/" . $ss_uri . "/html/" . $bl_name . ".html");
+    if (!isset($bl_mass[$bl_name])) {
+        $bl_mass[$bl_name]['m'] = file_exists("themes/".$ss_uri."/html/".$bl_name.".html");
     }
     
     // Если есть кастомный файл
     if ($bl_mass[$bl_name]['m'] ?? false) {
-        $file_content = file_get_contents("themes/" . $ss_uri . "/html/" . $bl_name . ".html");
+        $file_content = file_get_contents("themes/".$ss_uri."/html/".$bl_name.".html");
         
-        // Обрабатываем шаблон (eval для совместимости со старыми шаблонами)
-        $output = eval_block_template($file_content, $title, $content);
+        // Заменяем переменные в шаблоне
+        $file_content = str_replace(
+            ['$title', '$content', '$ss_uri', '{$title}', '{$content}', '{$ss_uri}'],
+            [$title, $content, $ss_uri, $title, $content, $ss_uri],
+            $file_content
+        );
         
         if ($pos == "o") {
-            return $output;
+            return $file_content;
         } else {
-            echo $output;
+            echo $file_content;
             return;
         }
     }
@@ -76,20 +93,31 @@ function themesidebox($title, $content, $pos) {
         case 'r': $std_name = "block-right"; break;
         case 'c': $std_name = "block-center"; break;
         case 'd': $std_name = "block-down"; break;
-        case 's': 
-        case 'o': $std_name = "block-fly"; break;
+        case 's': $std_name = "block-fly"; break;
+        case 'o': 
+            $func = 'return ';
+            $func2 = '';
+            $std_name = "block-fly";
+            break;
         default: $std_name = "block-all"; break;
     }
     
     // Инициализируем стандартный блок если нужно
-    if (!isset($bl_mass[$std_name]['m'])) {
-        $bl_mass[$std_name]['m'] = file_exists("themes/" . $ss_uri . "/html/" . $std_name . ".html");
+    if (!isset($bl_mass[$std_name])) {
+        $bl_mass[$std_name]['m'] = file_exists("themes/".$ss_uri."/html/".$std_name.".html");
         
         if ($bl_mass[$std_name]['m']) {
-            $f_str = file_get_contents("themes/" . $ss_uri . "/html/" . $std_name . ".html");
+            $f_str = file_get_contents("themes/".$ss_uri."/html/".$std_name.".html");
             
-            $bl_mass[$std_name]['f'] = function($title, $content) use ($f_str) {
-                return eval_block_template($f_str, $title, $content);
+            // Создаем анонимную функцию вместо устаревшей create_function
+            $bl_mass[$std_name]['f'] = function($title, $content) use ($f_str, $ss_uri) {
+                // Заменяем переменные в шаблоне
+                $result = str_replace(
+                    ['$title', '$content', '$ss_uri', '{$title}', '{$content}', '{$ss_uri}'],
+                    [$title, $content, $ss_uri, $title, $content, $ss_uri],
+                    $f_str
+                );
+                return $result;
             };
         }
     }
@@ -107,14 +135,20 @@ function themesidebox($title, $content, $pos) {
     }
     
     // Проверяем существование block-all как последний вариант
-    if (!isset($bl_mass['block-all']['m'])) {
-        $bl_mass['block-all']['m'] = file_exists("themes/" . $ss_uri . "/html/block-all.html");
+    if (!isset($bl_mass['block-all'])) {
+        $bl_mass['block-all']['m'] = file_exists("themes/".$ss_uri."/html/block-all.html");
         
         if ($bl_mass['block-all']['m']) {
-            $f_str = file_get_contents("themes/" . $ss_uri . "/html/block-all.html");
+            $f_str = file_get_contents("themes/".$ss_uri."/html/block-all.html");
             
-            $bl_mass['block-all']['f'] = function($title, $content) use ($f_str) {
-                return eval_block_template($f_str, $title, $content);
+            $bl_mass['block-all']['f'] = function($title, $content) use ($f_str, $ss_uri) {
+                // Заменяем переменные в шаблоне
+                $result = str_replace(
+                    ['$title', '$content', '$ss_uri', '{$title}', '{$content}', '{$ss_uri}'],
+                    [$title, $content, $ss_uri, $title, $content, $ss_uri],
+                    $f_str
+                );
+                return $result;
             };
         }
     }
@@ -132,7 +166,7 @@ function themesidebox($title, $content, $pos) {
     }
     
     // Фолбэк: простой HTML
-    $fallback_html = "<fieldset><legend>" . htmlspecialchars($title) . "</legend>" . $content . "</fieldset>";
+    $fallback_html = "<fieldset><legend>".htmlspecialchars($title)."</legend>".$content."</fieldset>";
     
     if ($pos == "o") {
         return $fallback_html;
@@ -141,96 +175,73 @@ function themesidebox($title, $content, $pos) {
     }
 }
 
-/**
- * Обработка шаблона блока через eval для совместимости
- */
-function eval_block_template(string $template, string $title, string $content): string {
-    global $ss_uri, $tracker_lang;
-    
-    // Экранируем кавычки для безопасного использования в eval
-    $escaped_template = str_replace(['"', '$'], ['\"', '\$'], $template);
-    
-    // Создаем строку для eval с подставленными переменными
-    $eval_code = 'return "' . $escaped_template . '";';
-    
-    // Выполняем eval с локальными переменными
-    return eval($eval_code);
-}
-
-/**
- * Альтернативная функция с шаблонными заменами (если не работает eval)
- */
-function simple_block_template(string $template, string $title, string $content): string {
-    global $ss_uri;
-    
-    // Базовые замены
-    $replacements = [
-        '{$title}' => $title,
-        '{$content}' => $content,
-        '{$ss_uri}' => $ss_uri,
-    ];
-    
-    // Добавляем замены для HTML-сущностей
-    $replacements['{$title|html}'] = htmlspecialchars($title);
-    $replacements['{$content|html}'] = htmlspecialchars($content);
-    
-    return str_replace(array_keys($replacements), array_values($replacements), $template);
-}
-
 function show_blocks($position) {
     global $CURUSER, $use_blocks, $already_used, $orbital_blocks;
-    static $showed_show_hide;
+    static $showed_show_hide = false;
 
-    if ($use_blocks) {
-        if (!$already_used) {
-            $blocks_res = sql_query("SELECT * FROM orbital_blocks WHERE active = 1 ORDER BY weight ASC") or sqlerr(__FILE__, __LINE__);
-            $orbital_blocks = [];
-            while ($blocks_row = mysqli_fetch_array($blocks_res)) {
-                $orbital_blocks[] = $blocks_row;
-            }
-            if (!$orbital_blocks) {
-                $orbital_blocks = [];
-            }
-            $already_used = true;
+    if (!$use_blocks) {
+        return;
+    }
+
+    if (!$already_used) {
+        $blocks_res = sql_query("SELECT * FROM orbital_blocks WHERE active = 1 ORDER BY weight ASC") or sqlerr(__FILE__, __LINE__);
+        
+        $orbital_blocks = [];
+        while ($blocks_row = mysqli_fetch_array($blocks_res)) {
+            $orbital_blocks[] = $blocks_row;
         }
+        
+        if (empty($orbital_blocks)) {
+            $orbital_blocks = [];
+        }
+        
+        $already_used = true;
+    }
 
-        foreach ($orbital_blocks as $block) {
-            if (!$showed_show_hide) {
-                echo '<script language="javascript" type="text/javascript" src="js/show_hide.js"></script>';
-            }
+    if (empty($orbital_blocks)) {
+        return;
+    }
+
+    foreach ($orbital_blocks as $block) {
+        if (!$showed_show_hide) {
+            echo '<script language="javascript" type="text/javascript" src="js/show_hide.js"></script>';
             $showed_show_hide = true;
+        }
+        
+        $bid = $block["bid"] ?? 0;
+        $content = $block["content"] ?? "";
+        $title = $block["title"] ?? "";
+        $blockfile = $block["blockfile"] ?? "";
+        $bposition = $block["bposition"] ?? "";
+        $allow_hide = ($block["allow_hide"] ?? "") == 'yes';
+        
+        if ($position != $bposition) {
+            continue;
+        }
+        
+        $view = $block["view"] ?? 0;
+        $which = explode(",", $block["which"] ?? "");
+        $module_name = str_replace(".php", "", basename($_SERVER["PHP_SELF"]));
+        
+        if (!(in_array($module_name, $which) || in_array("all", $which) || (in_array("ihome", $which) && $module_name == "index"))) {
+            continue;
+        }
+        
+        $show_block = false;
+        
+        switch($view) {
+            case 0: $show_block = true; break;
+            case 1: $show_block = isset($CURUSER); break;
+            case 2: $show_block = (get_user_class() >= UC_MODERATOR); break;
+            case 3: $show_block = (!isset($CURUSER) || get_user_class() >= UC_MODERATOR); break;
+        }
+        
+        if ($show_block) {
+            // Устанавливаем глобальные переменные для использования в themesidebox
+            global $b_id;
+            $b_id = $bid;
             
-            $bid = $block["bid"];
-            $content = $block["content"];
-            $title = $block["title"];
-            $blockfile = $block["blockfile"];
-            $bposition = $block["bposition"];
-            $allow_hide = $block["allow_hide"] == 'yes';
-            
-            if ($position != $bposition) {
-                continue;
-            }
-            
-            $view = $block["view"];
-            $which = explode(",", $block["which"]);
-            $module_name = str_replace(".php", "", basename($_SERVER["PHP_SELF"]));
-            
-            if (!(in_array($module_name, $which) || in_array("all", $which) || (in_array("ihome", $which) && $module_name == "index"))) {
-                continue;
-            }
-            
-            $show_block = false;
-            
-            switch($view) {
-                case 0: $show_block = true; break;
-                case 1: $show_block = isset($CURUSER); break;
-                case 2: $show_block = (get_user_class() >= UC_MODERATOR); break;
-                case 3: $show_block = (!isset($CURUSER) || get_user_class() >= UC_MODERATOR); break;
-            }
-            
-            if ($show_block) {
-                render_blocks($blockfile, $title, $content, $bid, $bposition, $allow_hide);
-            }
+            render_blocks($blockfile, $title, $content, $bid, $bposition, $allow_hide);
         }
     }
 }
