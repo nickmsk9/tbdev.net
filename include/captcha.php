2a58a30018ed0901e54d8f8c74b96628e6f25e61
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2025-12-17 13:12:59 +0300

    чинм капчту

--- include/captcha.php
+++ include/captcha.php
@@ -4,47 +4,54 @@
 if(!defined('IN_TRACKER'))
   die('Hacking attempt!');
 
-function create_captcha() {
-	global $_COOKIE_SALT;
-	//$randomstr = mksecret(5);
-	$randomstr = rand(10000, 99999);
-	$imagehash = md5($randomstr . $_COOKIE_SALT); // Additional security tightening
-	// Have to use MYsql_query to prohibit seeing imagestring in debugmode
-	mysql_query("INSERT INTO captcha SET imagehash = ".sqlesc($imagehash).", imagestring = ".sqlesc($randomstr).", dateline = ".sqlesc(time())) or sqlerr(__FILE__,__LINE__);
-	return $imagehash;
+function create_captcha(): string {
+    global $_COOKIE_SALT;
+    
+    // Генерация случайной строки для капчи
+    $randomstr = (string)rand(10000, 99999);
+    $imagehash = md5($randomstr . ($_COOKIE_SALT ?? ''));
+    
+    // Используем безопасное подключение к базе данных
+    sql_query("INSERT INTO captcha SET imagehash = ".sqlesc($imagehash).", 
+              imagestring = ".sqlesc($randomstr).", 
+              dateline = ".sqlesc(time()));
+    
+    return $imagehash;
 }
 
-function my_strlen($string) {
-	$string = preg_replace("#&\#(0-9]+);#", "-", $string);
-	if(function_exists("mb_strlen")) {
-		$string_length = mb_strlen($string);
-	} else {
-		$string_length = strlen($string);
-	}
+function my_strlen(string $string): int {
+    // Очистка строки от HTML-сущностей
+    $string = preg_replace("#&\#(0-9]+);#", "-", $string);
+    
+    if(function_exists("mb_strlen")) {
+        $string_length = mb_strlen($string);
+    } else {
+        $string_length = strlen($string);
+    }
 
-	return $string_length;
+    return $string_length;
 }
 
-function get_extension($file) {
-	return strtolower(my_substr(strrchr($file, "."), 1));
+function get_extension(string $file): string {
+    return strtolower(pathinfo($file, PATHINFO_EXTENSION));
 }
 
-function my_substr($string, $start, $length="") {
-	if(function_exists("mb_substr")) {
-		if($length != "") {
-			$cut_string = mb_substr($string, $start, $length);
-		} else {
-			$cut_string = mb_substr($string, $start);
-		}
-	} else {
-		if($length != "") {
-			$cut_string = substr($string, $start, $length);
-		} else {
-			$cut_string = substr($string, $start);
-		}
-	}
+function my_substr(string $string, int $start, ?int $length = null): string {
+    if(function_exists("mb_substr")) {
+        if($length !== null) {
+            $cut_string = mb_substr($string, $start, $length);
+        } else {
+            $cut_string = mb_substr($string, $start);
+        }
+    } else {
+        if($length !== null) {
+            $cut_string = substr($string, $start, $length);
+        } else {
+            $cut_string = substr($string, $start);
+        }
+    }
 
-	return $cut_string;
+    return $cut_string ?: '';
 }
 
 ?>
