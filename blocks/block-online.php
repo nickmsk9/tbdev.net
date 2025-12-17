6e0c75104e21d8afd786b7091f0f29f933dd6bf2
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2025-12-17 14:33:24 +0300

    улучшаем слегка блок

--- blocks/block-online.php
+++ blocks/block-online.php
@@ -1,85 +1,117 @@
 <?php
+declare(strict_types=1);
+
 if (!defined('BLOCK_FILE')) {
-    Header("Location: ../index.php");
+    header('Location: ../index.php');
     exit;
 }
 
 global $CURUSER, $use_sessions;
 
-$blocktitle = "Кто в онлайне";
+$blocktitle = 'Кто в онлайне';
+$dt = time() - 300; // 5 минут
 
-// Получаем последнего зарегистрированного пользователя
-$a = sql_query("SELECT id, username FROM users WHERE status='confirmed' ORDER BY id DESC LIMIT 1");
-if ($a && mysqli_num_rows($a) > 0) {
-    $latest_user_row = mysqli_fetch_assoc($a);
-    if ($CURUSER) {
-        $latestuser = "<a href=\"userdetails.php?id=" . $latest_user_row["id"] . "\" class=\"online\">" . htmlspecialchars($latest_user_row["username"]) . "</a>";
-    } else {
-        $latestuser = htmlspecialchars($latest_user_row['username']);
+// Последний зарегистрированный
+$latestuser = 'Нет пользователей';
+$resLatest = sql_query("SELECT id, username FROM users WHERE status='confirmed' ORDER BY id DESC LIMIT 1");
+if ($resLatest && mysqli_num_rows($resLatest) > 0) {
+    $lu = mysqli_fetch_assoc($resLatest);
+    $lid = (int)($lu['id'] ?? 0);
+    $lname = (string)($lu['username'] ?? '');
+    $lnameHtml = htmlspecialchars($lname, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+
+    if ($lid > 0 && $lname !== '') {
+        $latestuser = $CURUSER
+            ? '<a href="userdetails.php?id=' . $lid . '" class="online">' . $lnameHtml . '</a>'
+            : $lnameHtml;
     }
-} else {
-    $latestuser = "Нет пользователей";
+}
+if ($resLatest) {
+    mysqli_free_result($resLatest);
 }
 
-// Инициализируем переменные
+// Онлайн-лист и счётчики
 $title_who = [];
-$users = $guests = $staff = $total = 0;
+$users = 0;
+$guests = 0;
+$staff = 0;
+$total = 0;
 
-$dt = time() - 300;
+$sql = $use_sessions
+    ? "SELECT s.uid AS id, s.username, s.class FROM sessions AS s WHERE s.time > " . sqlesc($dt) . " ORDER BY s.class DESC"
+    : "SELECT u.id AS id, u.username, u.class FROM users AS u WHERE u.last_access > " . sqlesc($dt) . " ORDER BY u.class DESC";
 
-if ($use_sessions) {
-    // Используем сессии для отслеживания онлайн
-    $result = sql_query("SELECT s.uid, s.username, s.class FROM sessions AS s WHERE s.time > " . sqlesc($dt) . " ORDER BY s.class DESC");
-} else {
-    // Используем last_access в таблице users
-    $result = sql_query("SELECT u.id, u.username, u.class FROM users AS u WHERE u.last_access > " . sqlesc($dt) . " ORDER BY u.class DESC");
-}
+$result = sql_query($sql);
+
+$seenNames = [];
+$seenIds = [];
 
 if ($result) {
-    $parsed = [];
-    $parsed_id = [];
-
     while ($row = mysqli_fetch_assoc($result)) {
-        $uid = (int)$row['uid'] ?? (int)$row['id'] ?? 0;
-        $uname = $row['username'] ?? '';
-        $class = (int)$row['class'] ?? 0;
+        $uid   = (int)($row['id'] ?? 0);
+        $uname = (string)($row['username'] ?? '');
+        $class = (int)($row['class'] ?? 0);
 
-        // Добавляем пользователя в список "Кто здесь"
-        if (!empty($uname) && !in_array($uname, $parsed, true)) {
-            $parsed[] = $uname;
-            $title_who[] = "<a href=\"userdetails.php?id=" . $uid . "\" class=\"online\">" . get_user_class_color($class, htmlspecialchars($uname)) . "</a>";
+        // "Кто здесь" (уникально по username)
+        if ($uname !== '' && !isset($seenNames[$uname])) {
+            $seenNames[$uname] = true;
+            $unameHtml = htmlspecialchars($uname, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+            $title_who[] = '<a href="userdetails.php?id=' . $uid . '" class="online">' . get_user_class_color($class, $unameHtml) . '</a>';
         }
 
-        // Считаем статистику
-        if (!in_array($uid, $parsed_id, true)) {
+        // Статистика (уникально по id)
+        if ($uid > 0 && !isset($seenIds[$uid])) {
+            $seenIds[$uid] = true;
+
             if ($class >= UC_MODERATOR) {
                 $staff++;
-            } elseif (empty($uname)) {
+            } elseif ($uname === '') {
                 $guests++;
-            } elseif ($class < UC_MODERATOR) {
+            } else {
                 $users++;
             }
-            $parsed_id[] = $uid;
+
             $total++;
         }
     }
-    
     mysqli_free_result($result);
 }
 
-// Формируем контент блока
-$content = "<table border=\"0\" width=\"100%\"><tr valign=\"middle\"><td align=\"left\" class=\"embedded\"><b>Последний зарегистрированный: </b> $latestuser<hr></td></tr></table>\n";
+// --- ВЫВОД ---
 
-if (count($title_who) > 0) {
-    $content .= "<table border=\"0\" width=\"100%\"><tr valign=\"middle\"><td align=\"left\" class=\"embedded\"><b>Кто здесь: </b><hr></td></tr><tr><td class=\"embedded\">" . implode(", ", $title_who) . "<hr></td></tr></table>\n";
+$content  = '<table border="0" width="100%" cellspacing="0" cellpadding="3">';
+$content .= '<tr><td class="embedded"><b>Последний зарегистрированный:</b> ' . $latestuser . '</td></tr>';
+$content .= '<tr><td class="embedded"><hr></td></tr>';
+
+$content .= '<tr><td class="embedded"><b>Кто здесь:</b></td></tr>';
+$content .= '<tr><td class="embedded">';
+if (!empty($title_who)) {
+    $content .= implode(', ', $title_who);
 } else {
-    $content .= "<table border=\"0\" width=\"100%\"><tr valign=\"middle\"><td align=\"left\" class=\"embedded\"><b>Кто здесь: </b>Нет пользователей онлайн за последние 5 минут.<hr></td></tr></table>\n";
+    $content .= 'Нет пользователей онлайн за последние 5 минут.';
 }
+$content .= '</td></tr>';
+$content .= '<tr><td class="embedded"><hr></td></tr>';
 
-$content .= "<table border=\"0\" width=\"100%\"><tr valign=\"middle\"><td colspan=\"2\" align=\"left\" class=\"embedded\"><b>В онлайн: </b></td></tr>\n";
-$content .= "<tr><td class=\"embedded\"><img src=\"pic/info/admin.gif\" alt=\"Админ\"></td><td width=\"90%\" class=\"embedded\">Админы: $staff</td></tr>\n";
-$content .= "<tr><td class=\"embedded\"><img src=\"pic/info/member.gif\" alt=\"Пользователь\"></td><td width=\"90%\" class=\"embedded\">Пользователи: $users</td></tr>\n";
-$content .= "<tr><td class=\"embedded\"><img src=\"pic/info/guest.gif\" alt=\"Гость\"></td><td width=\"90%\" class=\"embedded\">Гости: $guests</td></tr>\n";
-$content .= "<tr><td class=\"embedded\"><img src=\"pic/info/group.gif\" alt=\"Всего\"></td><td width=\"90%\" class=\"embedded\">Всего: $total</td></tr></table>\n";
+$content .= '<tr><td class="embedded"><b>В онлайн:</b></td></tr>';
+$content .= '</table>';
 
+$content .= '<table border="0" width="100%" cellspacing="0" cellpadding="3">';
+$content .= '<tr>'
+    . '<td class="embedded" width="1%"><img src="pic/info/admin.gif" alt="Админ"></td>'
+    . '<td class="embedded" width="99%">Админы: ' . (int)$staff . '</td>'
+    . '</tr>';
+$content .= '<tr>'
+    . '<td class="embedded" width="1%"><img src="pic/info/member.gif" alt="Пользователь"></td>'
+    . '<td class="embedded" width="99%">Пользователи: ' . (int)$users . '</td>'
+    . '</tr>';
+$content .= '<tr>'
+    . '<td class="embedded" width="1%"><img src="pic/info/guest.gif" alt="Гость"></td>'
+    . '<td class="embedded" width="99%">Гости: ' . (int)$guests . '</td>'
+    . '</tr>';
+$content .= '<tr>'
+    . '<td class="embedded" width="1%"><img src="pic/info/group.gif" alt="Всего"></td>'
+    . '<td class="embedded" width="99%"><b>Всего: ' . (int)$total . '</b></td>'
+    . '</tr>';
+$content .= '</table>';
 ?>
