2cb4e3616a68c79f5d26dbfdd52a98300eaf1d08
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2025-12-17 09:16:57 +0300

    1. Безопасная обработка входных данных Добавлена проверка isset() для $_GET переменных
    Использован оператор объединения с null (??)
    
    Приведен $page к integer
    
    2. Исправлена работа с UTF-8
    Заменен htmlspecialchars_uni() на htmlspecialchars(..., ENT_QUOTES, 'UTF-8')
    
    Использованы multibyte функции для русского алфавита
    
    3. Исправлены потенциальные ошибки
    Убраны невалидные даты 0000-00-00 00:00:00
    
    Исправлен расчет рейтинга (добавлен min() для ограничения)
    
    Проверка существования данных перед использованием
    
    4. Улучшена безопасность SQL
    Использованы подготовленные параметры (через функцию sql_query())
    
    Экранирование специальных символов в URL
    
    5. Оптимизирована логика
    Убраны закомментированные блоки
    
    Улучшена структура условий
    
    Добавлена проверка на пустой результат

--- users.php
+++ users.php
@@ -26,172 +26,266 @@
 // +--------------------------------------------------------------------------+
 */
 
-require "include/bittorrent.php";
-
+require_once "include/bittorrent.php";
 dbconn();
-
 loggedinorreturn();
 
-$search = trim($_GET['search']);
-$class = $_GET['class'];
-if ($class == '-' || !is_valid_user_class($class))
-	$class = '';
+// Инициализация переменных
+$search = isset($_GET['search']) ? trim($_GET['search']) : '';
+$class = $_GET['class'] ?? '';
+$letter = $_GET['letter'] ?? '';
+$page = (int)($_GET['page'] ?? 1);
 
-if ($search != '' || $class) {
-	$query = "username LIKE '%" . sqlwildcardesc("$search") . "%' AND status='confirmed'";
-	if ($search)
-		$q = "search=" . htmlspecialchars_uni($search);
-} else {
-	$letter = trim($_GET["letter"]);
-	if (strlen($letter) > 1)
-		die;
-
-/*	if ($letter != "" && strpos("abcdefghijklmnopqrstuvwxyz", $letter) === false)
-		$letter = "a";
-	$query = ( $letter != "" ? "username LIKE '$letter%' AND " : "") . "status='confirmed'";
-	if ($letter == "")
-		$letter = "a";
-	$q = "letter=$letter";*/
-
-	if ($letter != "" && strpos("abcdefghijklmnopqrstuvwxyz" . "абвгдеёжзийклмнопрстуфхцчшщъыьэюя", $letter) === false)
-		$letter = "";
-	$query = ( $letter != "" ? "username LIKE '$letter%' AND " : "") . "status='confirmed'";
-	if ($letter != "")
-		$q = "letter=$letter";
-
+// Валидация класса
+if ($class == '-' || !is_valid_user_class($class)) {
+    $class = '';
 }
 
+// Построение запроса
+$where_parts = ["status='confirmed'"];
+$params = [];
+$query_params = [];
+
+// Обработка поиска
+if ($search != '') {
+    $where_parts[] = "username LIKE ?";
+    $params[] = '%' . $search . '%';
+    $query_params['search'] = htmlspecialchars($search, ENT_QUOTES, 'UTF-8');
+}
+
+// Обработка фильтра по букве
+if ($search == '' && !$class && $letter != '') {
+    // Безопасная проверка буквы
+    $letter = mb_strtolower($letter, 'UTF-8');
+    $valid_letters = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+    
+    if (mb_strlen($letter, 'UTF-8') == 1 && mb_strpos($valid_letters, $letter, 0, 'UTF-8') !== false) {
+        $where_parts[] = "username LIKE ?";
+        $params[] = $letter . '%';
+        $query_params['letter'] = htmlspecialchars($letter, ENT_QUOTES, 'UTF-8');
+    } else {
+        $letter = '';
+    }
+}
+
+// Добавление фильтра по классу
 if (is_valid_user_class($class)) {
-	$query .= " AND class = $class";
-	$q .= ($q ? "&amp;" : "") . "class=$class";
+    $where_parts[] = "class = ?";
+    $params[] = $class;
+    $query_params['class'] = $class;
 }
 
+// Сборка WHERE-условия
+$where = implode(' AND ', $where_parts);
+
+// Подготовка query string для ссылок
+$q = '';
+foreach ($query_params as $key => $value) {
+    $q .= ($q ? '&amp;' : '') . $key . '=' . $value;
+}
+
+// Пагинация
+$perpage = 100;
+
+// Получение общего количества записей
+$count_sql = "SELECT COUNT(*) FROM users WHERE $where";
+$count_res = sql_query($count_sql, $params) or sqlerr(__FILE__, __LINE__);
+$count_arr = mysqli_fetch_row($count_res);
+$total = (int)$count_arr[0];
+$pages = (int)ceil($total / $perpage);
+
+// Корректировка номера страницы
+$page = max(1, min($page, $pages));
+
+// Вывод HTML
 stdhead("Пользователи");
 
 print("<h1>Пользователи</h1>\n");
 
+// Форма поиска
 print("<form method=\"get\" action=\"users.php\">\n");
-print("Поиск: <input type=\"text\" size=\"30\" name=\"search\" value=\"".htmlspecialchars_uni($search)."\">\n");
+print("Поиск: <input type=\"text\" size=\"30\" name=\"search\" value=\"" . htmlspecialchars($search, ENT_QUOTES, 'UTF-8') . "\">\n");
 print("<select name=\"class\">\n");
 print("<option value=\"-\">(Все уровни)</option>\n");
-for ($i = 0;;++$i) {
-if ($c = get_user_class_name($i))
-	print("<option value=\"$i\"" . (is_valid_user_class($class) && $class == $i ? " selected" : "") . ">$c</option>\n");
-else
-	break;
+
+for ($i = 0; ; ++$i) {
+    if ($c = get_user_class_name($i)) {
+        $selected = (is_valid_user_class($class) && $class == $i) ? " selected" : "";
+        print("<option value=\"$i\"$selected>" . htmlspecialchars($c, ENT_QUOTES, 'UTF-8') . "</option>\n");
+    } else {
+        break;
+    }
 }
+
 print("</select>\n");
 print("<input type=\"submit\" value=\"Вперед\">\n");
 print("</form>\n");
 
+// Буквы латинского алфавита
 print("<p>\n");
-
-for ($i = 97; $i < 123; ++$i)
-{
-$l = chr($i);
-$L = chr($i - 32);
-if ($l == $letter)
-print("<b>$L</b>\n");
-else
-print("<a href=\"users.php?letter=$l\"><b>$L</b></a>\n");
+for ($i = 97; $i < 123; ++$i) {
+    $l = chr($i);
+    $L = chr($i - 32);
+    $class_active = ($l == $letter) ? ' active' : '';
+    
+    if ($l == $letter) {
+        print("<b>$L</b>\n");
+    } else {
+        $url = "users.php?letter=" . urlencode($l);
+        if ($class) $url .= "&amp;class=" . urlencode($class);
+        if ($search) $url .= "&amp;search=" . urlencode($search);
+        print("<a href=\"$url\"><b>$L</b></a>\n");
+    }
 }
-
 print("</p>\n");
 
+// Буквы русского алфавита
 print("<p>\n");
+$russian_letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+$russian_uppercase_letters = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
-$russian_letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"; // Да, Я догадываюсь что слова не могут начинатся с ьъы, но Юзернеймы могут!
-$russian_upperscase_letters = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-foreach (str_split($russian_letters) as $key => $l)
-{
-$L = $russian_upperscase_letters[$key];
-if ($l == $letter)
-print("<b>$L</b>\n");
-else
-print("<a href=\"users.php?letter=$l\"><b>$L</b></a>\n");
+for ($i = 0; $i < mb_strlen($russian_letters, 'UTF-8'); $i++) {
+    $l = mb_substr($russian_letters, $i, 1, 'UTF-8');
+    $L = mb_substr($russian_uppercase_letters, $i, 1, 'UTF-8');
+    
+    if ($l == $letter) {
+        print("<b>$L</b>\n");
+    } else {
+        $url = "users.php?letter=" . urlencode($l);
+        if ($class) $url .= "&amp;class=" . urlencode($class);
+        if ($search) $url .= "&amp;search=" . urlencode($search);
+        print("<a href=\"$url\"><b>$L</b></a>\n");
+    }
 }
-
 print("</p>\n");
 
-$q .= ($q ? "&amp;" : "");
-$page = $_GET['page'];
-$perpage = 100;
+// Пагинация вверху
+$pagemenu = '';
+$browsemenu = '';
 
-$res = sql_query("SELECT COUNT(*) FROM users WHERE $query") or sqlerr(__FILE__, __LINE__);
-$arr = mysql_fetch_row($res);
-$pages = floor($arr[0] / $perpage);
-if ($pages * $perpage < $arr[0])
-++$pages;
+// Номера страниц
+for ($i = 1; $i <= $pages; ++$i) {
+    if ($i == $page) {
+        $pagemenu .= "<b>$i</b>\n";
+    } else {
+        $url = "users.php?page=$i";
+        if ($q) $url .= '&amp;' . $q;
+        $pagemenu .= "<a href=\"$url\"><b>$i</b></a>\n";
+    }
+}
 
-if ($page < 1)
-$page = 1;
-else
-if ($page > $pages)
-$page = $pages;
-
-for ($i = 1; $i <= $pages; ++$i)
-if ($i == $page)
-$pagemenu .= "<b>$i</b>\n";
-else
-$pagemenu .= "<a href=\"users.php?{$q}page=$i\"><b>$i</b></a>\n";
-
-if ($page == 1)
-$browsemenu .= "<b>&lt;&lt; Пред</b>";
-else
-$browsemenu .= "<a href=\"users.php?{$q}page=" . ($page - 1) . "\"><b>&lt;&lt; Пред</b></a>";
+// Кнопки "Назад/Вперед"
+if ($page == 1) {
+    $browsemenu .= "<b>&lt;&lt; Пред</b>";
+} else {
+    $prev = $page - 1;
+    $url = "users.php?page=$prev";
+    if ($q) $url .= '&amp;' . $q;
+    $browsemenu .= "<a href=\"$url\"><b>&lt;&lt; Пред</b></a>";
+}
 
 $browsemenu .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
 
-if ($page == $pages || (($page * $perpage) > $arr[0]))
-$browsemenu .= "<b>След &gt;&gt;</b>";
-else
-$browsemenu .= "<a href=\"users.php?{$q}page=" . ($page + 1) . "\"><b>След &gt;&gt;</b></a>";
+if ($page == $pages || ($page * $perpage) >= $total) {
+    $browsemenu .= "<b>След &gt;&gt;</b>";
+} else {
+    $next = $page + 1;
+    $url = "users.php?page=$next";
+    if ($q) $url .= '&amp;' . $q;
+    $browsemenu .= "<a href=\"$url\"><b>След &gt;&gt;</b></a>";
+}
 
 print("<p>$browsemenu<br />$pagemenu</p>");
 
-$offset = ($page * $perpage) - $perpage;
+// Получение данных
+$offset = ($page - 1) * $perpage;
+$limit_sql = "LIMIT $offset, $perpage";
+$data_sql = "SELECT u.*, c.name, c.flagpic 
+             FROM users AS u 
+             LEFT JOIN countries AS c ON c.id = u.country 
+             WHERE $where 
+             ORDER BY username 
+             $limit_sql";
 
-$res = sql_query("SELECT u.*, c.name, c.flagpic FROM users AS u LEFT JOIN countries AS c ON c.id = u.country WHERE $query ORDER BY username LIMIT $offset,$perpage") or sqlerr(__FILE__, __LINE__);
-$num = mysql_num_rows($res);
+$res = sql_query($data_sql, $params) or sqlerr(__FILE__, __LINE__);
+$num = mysqli_num_rows($res);
 
-print("<table border=\"1\" cellspacing=\"0\" cellpadding=\"5\">\n");
-print("<tr><td class=\"colhead\" align=\"left\">Имя</td><td class=\"colhead\">Зарегестрирован</td><td class=\"colhead\">Последний вход</td><td class=\"colhead\">Рейтинг</td><td class=\"colhead\">Пол</td><td class=\"colhead\" align=\"left\">Уровень</td><td class=\"colhead\">Страна</td></tr>\n");
-for ($i = 0; $i < $num; ++$i)
-{
-$arr = mysql_fetch_assoc($res);
-if ($arr['country'] > 0) {
-$country = "<td style=\"padding: 0px\" align=\"center\"><img src=\"pic/flag/$arr[flagpic]\" alt=\"$arr[name]\" title=\"$arr[name]\"></td>";
+// Таблица с пользователями
+if ($num > 0) {
+    print("<table border=\"1\" cellspacing=\"0\" cellpadding=\"5\">\n");
+    print("<tr>
+            <td class=\"colhead\" align=\"left\">Имя</td>
+            <td class=\"colhead\">Зарегистрирован</td>
+            <td class=\"colhead\">Последний вход</td>
+            <td class=\"colhead\">Рейтинг</td>
+            <td class=\"colhead\">Пол</td>
+            <td class=\"colhead\" align=\"left\">Уровень</td>
+            <td class=\"colhead\">Страна</td>
+          </tr>\n");
+
+    while ($arr = mysqli_fetch_assoc($res)) {
+        // Обработка дат
+        $added = ($arr['added'] == '0000-00-00 00:00:00') ? '-' : htmlspecialchars($arr['added'], ENT_QUOTES, 'UTF-8');
+        $last_access = ($arr['last_access'] == '0000-00-00 00:00:00') ? '-' : htmlspecialchars($arr['last_access'], ENT_QUOTES, 'UTF-8');
+        
+        // Расчет рейтинга
+        $ratio = "---";
+        if ($arr["downloaded"] > 0) {
+            $ratio_num = $arr["uploaded"] / $arr["downloaded"];
+            $ratio = number_format(min($ratio_num, 100), 2);
+            if ($ratio_num > 100) {
+                $ratio = "100+";
+            }
+            $ratio = "<font color=\"" . get_ratio_color($ratio) . "\">$ratio</font>";
+        } elseif ($arr["uploaded"] > 0) {
+            $ratio = "∞";
+        }
+        
+        // Пол
+        $gender = "";
+        if ($arr["gender"] == "1") {
+            $gender = "<img src=\"" . $pic_base_url . "/male.gif\" alt=\"Мужской\" title=\"Мужской\">";
+        } elseif ($arr["gender"] == "2") {
+            $gender = "<img src=\"" . $pic_base_url . "/female.gif\" alt=\"Женский\" title=\"Женский\">";
+        }
+        
+        // Страна
+        $country = "<td align=\"center\">---</td>";
+        if ($arr['country'] > 0 && !empty($arr['flagpic'])) {
+            $country_name = htmlspecialchars($arr['name'] ?? '', ENT_QUOTES, 'UTF-8');
+            $country = "<td style=\"padding: 0px\" align=\"center\">
+                        <img src=\"pic/flag/{$arr['flagpic']}\" alt=\"$country_name\" title=\"$country_name\">
+                        </td>";
+        }
+        
+		
+        // Имя пользователя с цветом класса
+        $username = get_user_class_color($arr["class"], htmlspecialchars($arr["username"], ENT_QUOTES, 'UTF-8'));
+		$donated = isset($arr["donated"]) ? (int)$arr["donated"] : 0;
+		$donor_icon = ($donated > 0) ? "<img src=\"pic/star.gif\" border=\"0\" alt=\"Донатер\" title=\"Донатер\">" : "";
+
+        // Класс
+        $user_class = get_user_class_name($arr["class"]);
+        $user_class_display = htmlspecialchars($user_class, ENT_QUOTES, 'UTF-8');
+        
+        print("<tr>
+                <td align=\"left\">
+                    <a href=\"userdetails.php?id={$arr['id']}\"><b>$username</b></a>$donor_icon
+                </td>
+                <td>$added</td>
+                <td>$last_access</td>
+                <td>$ratio</td>
+                <td>$gender</td>
+                <td align=\"left\">$user_class_display</td>
+                $country
+              </tr>\n");
+    }
+    print("</table>\n");
+} else {
+    print("<p>Пользователи не найдены.</p>\n");
 }
-else
-	$country = "<td align=\"center\">---</td>";
-if ($arr['added'] == '0000-00-00 00:00:00')
-	$arr['added'] = '-';
-if ($arr['last_access'] == '0000-00-00 00:00:00')
-	$arr['last_access'] = '-';
-if ($arr["downloaded"] > 0) {
-	$ratio = number_format($arr["uploaded"] / $arr["downloaded"], 2);
-if (($arr["uploaded"] / $arr["downloaded"]) > 100)
-	$ratio = "100+";
-$ratio = "<font color=\"" . get_ratio_color($ratio) . "\">$ratio</font>";
-}
-else
-	if ($arr["uploaded"] > 0)
-		$ratio = "Inf.";
-	else
-		$ratio = "------";
 
-if ($arr["gender"] == "1") $gender = "<img src=\"".$pic_base_url."/male.gif\" alt=\"Парень\" style=\"margin-left: 4pt\">";
-elseif ($arr["gender"] == "2") $gender = "<img src=\"".$pic_base_url."/female.gif\" alt=\"Девушка\" style=\"margin-left: 4pt\">";
-
-print("<tr><td align=\"left\"><a href=\"userdetails.php?id=$arr[id]\"><b>".get_user_class_color($arr["class"], $arr["username"])."</b></a>" .($arr["donated"] > 0 ? "<img src=\"pic/star.gif\" border=\"0\" alt=\"Donor\">" : "")."</td>" .
-"<td>$arr[added]</td><td>$arr[last_access]</td><td>$ratio</td><td>$gender</td>".
-"<td align=\"left\">" . get_user_class_name($arr["class"]) . "</td>$country</tr>\n");
-}
-print("</table>\n");
-
+// Пагинация внизу
 print("<p>$pagemenu<br />$browsemenu</p>");
 
 stdfoot();
-die;
-
-?>
